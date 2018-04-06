0ef33f1cefa6f277439a52fd2cbc33a145bdf43e
waynelijia_i <waynelijia_i@didichuxing.com>
2018-04-06 15:06:50 +0800

    init

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', function (req, res) {
+  
+})
+
+app.listen(9000, function () {
+  console.log('server is running 9000')
+})
